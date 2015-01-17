5fc90186a1ca186ea5f8755ee6b853776f3a92b4
tamblc <tamblc@rpi.edu>
2015-01-17 09:27:23 -0500

    I GOT IT TO DO A THING!
    AND THE THING IS GOOD!

--- js/map/controls.js
+++ js/map/controls.js
@@ -1,11 +1,18 @@
+
+
 var add = document.getElementById('addButton');
 
-add.onclick(function()
+console.log("control.js hit")
+
+
+
+
+add.onclick = function()
 {
-	
-});
-
-
-var map = document.getElementById('map');
-
-omnivore.csv('/NeedATampon/core/data/points.csv').addTo(map);
+	L.mapbox.accessToken = 'pk.eyJ1IjoidGFtYmxjIiwiYSI6IjZYMlpVME0ifQ.SkFvnDEfXGkR2gLzVC6GXg';
+	var map  = L.mapbox.map('map', 'tamblc.kpa72c76');
+	console.log( L.control.locate());
+	L.control.locate().addTo(map);
+	map.setView([40, -74.50], 9);
+	console.log('hit');
+}
